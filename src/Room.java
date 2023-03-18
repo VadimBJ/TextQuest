c9796a2a683ab43775bac714491c890f2f3818c8
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-18 18:07:17 +0100

    add getter

--- src/Room.java
+++ src/Room.java
@@ -1,13 +1,22 @@
 public class Room {
   private final String title;
-  private String roomDescription;
+  private final String roomDescription;
   private int column;
   private int row;
 
-  public Room(String title, int column, int row) {
+  public Room(String title, int column, int row, String roomDescription) {
     this.title = title;
     this.column = column;
     this.row = row;
+    this.roomDescription = roomDescription;
+  }
+
+  public String getRoomDescription() {
+    return roomDescription;
+  }
+
+  public String getTitle() {
+    return title;
   }
 
   public void setRow(int row) {
@@ -26,7 +35,4 @@ public class Room {
     return column;
   }
 
-  public void setRoomDescription(String roomDescription) {
-    this.roomDescription = roomDescription;
-  }
 }
